81e6513eba3dd555a13550a2b3bb6f3c70ec0a97
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-05-28 20:08:29 +0900

    Add Cors

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 8000;
 const uploadRouter = require("./src/Router/file");
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // File Upload
 app.use("/files", uploadRouter);
 
