6f99c016f43ceef462e518614ff9cb5faf01fe6c
ricecha <sycha2000@gmail.com>
2023-05-31 23:37:33 +0900

    error fix

--- trash+translate/file-trash-service/src/main/java/com/ricecha/s3/FileTrashServiceApplication.java
+++ trash+translate/file-trash-service/src/main/java/com/ricecha/s3/FileTrashServiceApplication.java
@@ -1,23 +1,12 @@
 package com.ricecha.s3;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.ricecha.s3.service.TranslationService;
 
 @SpringBootApplication
 public class FileTrashServiceApplication {
 
 	public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(FileTrashServiceApplication.class, args);
-
-        TranslationService translationService = context.getBean(TranslationService.class);
-
-        String filePath = "path_to_your_file";
-        translationService.translateAndExportToS3(filePath);
-
-        context.close();
+        
     }
 
 }
--- trash+translate/file-trash-service/src/test/java/com/ricecha/s3/FileTrashServiceApplicationTests.java
+++ trash+translate/file-trash-service/src/test/java/com/ricecha/s3/FileTrashServiceApplicationTests.java
@@ -4,16 +4,44 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.context.ConfigurableApplicationContext;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import com.ricecha.s3.service.StorageService;
 import com.ricecha.s3.service.TranslationService;
 
-@SpringBootTest
+@Configuration
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class FileTrashServiceApplicationTests {
 
 	@Autowired
     private StorageService service;
+	
+	 @TestConfiguration
+	 @ComponentScan(basePackages = "com.ricecha.s3.service")
+	 static class TestConfig {
+	     // You can define any additional beans required for testing here
+	 }
+
+	 @Bean
+	 TranslationService translationService() {
+	     return new TranslationService();
+	 }
+	
+	void dtestTranslateAndExportToS3()
+	{
+		ConfigurableApplicationContext context = SpringApplication.run(FileTrashServiceApplicationTests.class);
+
+        TranslationService translationService = context.getBean(TranslationService.class);
+
+        String filePath = "path_to_your_file";
+        translationService.translateAndExportToS3(filePath);
+
+        context.close();
+	}
 	
 	@Test
     void deleteFileTest() {
