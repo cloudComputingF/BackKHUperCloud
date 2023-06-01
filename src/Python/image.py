000f1f1c3538b5f9c5dd9712f6ad2bc2afe2e7ef
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-06-01 15:02:54 +0900

    Convert URL to ImAGE

--- src/Python/image.py
+++ src/Python/image.py
@@ -1,1 +1,24 @@
 # For 중복 이미지 처리
+photo_list = []
+
+"""
+S3 로 파일 불러오기.
+
+
+
+"""
+# pip install opencv-python
+import cv2
+from skimage.metrics import structural_similarity as ssim
+
+import urllib
+import numpy as np
+
+
+# S3 Object URL -> Convert -> IMAGE
+def url_to_image(URL):
+    resp = urllib.urlopen(URL)
+    image = np.asarray(bytearray(resp.read()), dtype="unint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    return image
