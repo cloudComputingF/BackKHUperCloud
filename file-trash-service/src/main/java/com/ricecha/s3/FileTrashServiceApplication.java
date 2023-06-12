6a52e49eb99db408edfe845be8635c119a1fcc92
ricecha <sycha2000@gmail.com>
2023-06-12 20:27:35 +0900

    cors error

--- file-trash-service/src/main/java/com/ricecha/s3/FileTrashServiceApplication.java
+++ file-trash-service/src/main/java/com/ricecha/s3/FileTrashServiceApplication.java
@@ -2,6 +2,9 @@ package com.ricecha.s3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FileTrashServiceApplication {
@@ -9,5 +12,15 @@ public class FileTrashServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FileTrashServiceApplication.class, args);
 	}
+	
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**").allowedOrigins("http://front-server.com");
+	            }
+	        };
+	    }
 
 }
